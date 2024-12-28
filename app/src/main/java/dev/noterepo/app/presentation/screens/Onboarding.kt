ba1e08bb637dbca77eb4380de1ebdee2dd0b6840
= <ssh.xero@gmail.com>
2024-12-28 22:43:58 +0100

    feat(ui): Improve onboarding screen

--- app/src/main/java/dev/noterepo/app/presentation/screens/Onboarding.kt
+++ app/src/main/java/dev/noterepo/app/presentation/screens/Onboarding.kt
@@ -1,5 +1,6 @@
 package dev.noterepo.app.presentation.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.IconButtonDefaults
@@ -16,6 +18,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -38,6 +41,15 @@ fun OnboardingScreen(modifier: Modifier = Modifier) {
                     .align(Alignment.BottomCenter)
                     .padding(horizontal = 12.dp, vertical = 12.dp)
             ) {
+                Image(
+                    painter = painterResource(R.drawable.noterepo_logo_white),
+                    contentDescription = "NoteRepo",
+                    modifier = Modifier
+                        .width(200.dp)
+                )
+
+                Spacer(modifier = Modifier.height(32.dp))
+
                 Text(
                     text = stringResource(R.string.onboarding_heading),
                     style = Typography.headlineMedium,
