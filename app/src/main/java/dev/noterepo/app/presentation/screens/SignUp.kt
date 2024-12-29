8e1d2ef02bff9c0278c25f9eaf5be51fd2fb2676
= <ssh.xero@gmail.com>
2024-12-29 01:05:37 +0100

    chore: Tidy up source code

--- app/src/main/java/dev/noterepo/app/presentation/screens/SignUp.kt
+++ app/src/main/java/dev/noterepo/app/presentation/screens/SignUp.kt
@@ -17,7 +17,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
